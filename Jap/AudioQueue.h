a7bc826a3b39741a4c150eda50ceb194ebf91b93
applede <jksong3@gmail.com>
2014-03-21 20:54:32 +0900

    moved audio function to AudioQueue

--- Jap/AudioQueue.h
+++ Jap/AudioQueue.h
@@ -7,15 +7,50 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AudioUnit/AudioUnit.h>
 #import <libavcodec/avcodec.h>
 #import <libavformat/avformat.h>
+#import "PacketQueue.h"
+
+typedef struct {
+  int freq;
+  int channels;
+  int64_t channel_layout;
+  enum AVSampleFormat fmt;
+  int frame_size;
+  int bytes_per_sec;
+} AudioParams;
 
 @interface AudioQueue : NSObject
 {
+  AudioComponentInstance _audioC;
+  
+  AVPacket _audio_pkt_temp;
+  AVPacket _audio_pkt;
+  int _audio_buf_frames_pending;
+  AVFrame *_frame;
+  int _paused;
+  int _audio_finished;
+  int _audio_pkt_temp_serial;
+  int64_t _audio_frame_next_pts;
+  AudioParams _audio_src;
+  AudioParams _audio_tgt;
+  struct SwrContext *_swr_ctx;
+  uint8_t *_audio_buf;
+  uint8_t *_audio_buf1;
+  unsigned int _audio_buf1_size;
+  double _audio_clock;
+  int _audio_clock_serial;
+  int _audio_buf_index;
+  int _audio_buf_size;
 }
 
 @property AVStream* stream;
+@property PacketQueue* packetQ;
 
-- (void)put:(AVPacket*)packet;
+- (void)start;
+- (void)stop;
+- (BOOL)prepare;
+- (void)close;
 
 @end
